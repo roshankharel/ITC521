c76f624299da6d1895ac76de538d6b4d78f60140
Roshan Kharel <rosn.khrl@gmail.com>
2020-10-13 19:22:41 +1100

    A3 - T3 thread implementation for animation

--- A3/T3/src/main/BallPane.java
+++ A3/T3/src/main/BallPane.java
@@ -1,67 +1,134 @@
 package main;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Platform;
-import javafx.beans.property.DoubleProperty;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.util.Duration;
 
-public class BallPane extends Pane {
-    public final double radius = 20;
-    private double x = radius, y = radius;
-    private double dx = 1, dy = 1;
-    private final Circle circle = new Circle(x, y, radius);
-    private final Timeline animation;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * BallPane
+ *
+ * BallPane is the primary pane for this application and is responsible
+ * for animating the ball movement. It provides methods to increase or
+ * decrease the speed of the ball and pause or play/resume the ball
+ * animation
+ */
+public class BallPane extends BorderPane {
+    public final double ballRadius = 20;
+    private double xPosition = ballRadius;
+    private double yPosition = ballRadius;
+    private double xDirection = 1;
+    private double yDirection = 1;
+
+    private final Circle circle;
+    private final AtomicBoolean paused;
+    private final AtomicInteger speed;
+
+    private SpeedChangedListener speedChangedListener;
 
     public BallPane() {
-        circle.setFill(Color.GRAY); // Set ball color
+        // vars initialization
+        circle = new Circle(xPosition, yPosition, ballRadius);
+        paused = new AtomicBoolean(false);
+        speed = new AtomicInteger(100);
 
-        getChildren().add(circle); // Place a ball into this pane
+        // Set ball color
+        circle.setFill(Color.web("#311b92"));
 
-        // Create an animation for moving the ball
-        animation = new Timeline(new KeyFrame(Duration.millis(50),
-                e -> Platform.runLater(this::moveBall)));
-        animation.setCycleCount(Timeline.INDEFINITE);
-        animation.play(); // Start animation
+        // add the ball node into this pane
+        getChildren().add(circle);
+
+        play();
     }
 
     public void play() {
-        animation.play();
+        paused.set(false);
+        initializeAnimationThread();
     }
 
     public void pause() {
-        animation.pause();
+        paused.set(true);
+    }
+
+    public boolean isPaused() {
+        return paused.get();
     }
 
     public void increaseSpeed() {
-        animation.setRate(animation.getRate() + 0.1);
+        speed.set(speed.get() + 5);
+
+        speedChangedListener.inform();
     }
 
     public void decreaseSpeed() {
-        animation.setRate(animation.getRate() > 0 ? (animation.getRate() - 0.1) : 0);
+        speed.set(Math.max(5, speed.get() - 5));
+        speedChangedListener.inform();
     }
 
-    public DoubleProperty rateProperty() {
-        return animation.rateProperty();
+    public double getSpeed() {
+        return speed.get() / 100.0;
     }
 
-    protected void moveBall() {
-        // Check boundaries
-        if (x < radius || x > getWidth() - radius) {
-            dx *= -1; // Change ball move direction
+    public void setOnSpeedChanged(SpeedChangedListener speedChangedListener) {
+        this.speedChangedListener = speedChangedListener;
+    }
+
+    protected synchronized void moveBall() {
+        // on collision with the boundary change ball direction
+        if (xPosition < ballRadius || xPosition > getWidth() - ballRadius) {
+            xDirection *= -1; // reverse ball movement direction in x-axis
         }
 
-        if (y < radius || y > getHeight() - radius) {
-            dy *= -1; // Change ball move direction
+        if (yPosition < ballRadius || yPosition > getHeight() - ballRadius) {
+            yDirection *= -1; // reverse ball movement direction in y-axis
         }
 
-        // Adjust ball position
-        x += dx;
-        y += dy;
-        circle.setCenterX(x);
-        circle.setCenterY(y);
+        // compute new position for the ball
+        xPosition += xDirection * getSpeed() * 0.1;
+        yPosition += yDirection * getSpeed() * 0.1;
+
+        // apply the position
+        circle.setCenterX(xPosition);
+        circle.setCenterY(yPosition);
+    }
+
+    private void initializeAnimationThread() {
+        // initialize animation thread
+        Thread animationThread = new Thread(() -> {
+            try {
+                // run until the game is paused
+                while (!paused.get()) {
+                    // ball must be moved javafx thread for the animation to run smoothly
+                    Platform.runLater(this::moveBall);
+                    //sleep thread for 5 milli seconds
+                    Thread.sleep(5);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        // makes thread terminate with the application
+        // else the thread keep on running even after the application
+        // is closed
+        animationThread.setDaemon(true);
+
+        // setting highest priority for the thread
+        animationThread.setPriority(Thread.MAX_PRIORITY);
+
+        // start the thread
+        animationThread.start();
+    }
+
+    /**
+     * SpeedChangedListener
+     *
+     * An interface for ball speed changed listener
+     */
+    public interface SpeedChangedListener {
+        void inform();
     }
 }
--- A3/T3/src/main/BounceBallControl.java
+++ A3/T3/src/main/BounceBallControl.java
@@ -2,50 +2,99 @@ package main;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
+/**
+ * BounceBallControl BounceBallControl is a JavaFX application which displays a animation of a
+ * circle moving between the its window boundaries.
+ *
+ * @author Roshan Kharel
+ */
 public class BounceBallControl extends Application {
+    private BallPane ballPane;
+    private Label speedLabel;
+
     @Override
     public void start(Stage primaryStage) {
-        BallPane ballPane = new BallPane(); // Create a ball pane
-        Label label = new Label(String.format("%.2f", ballPane.rateProperty().getValue()));
-        label.setPadding(new Insets(4, 8, 4, 8));
-        label.setStyle("-fx-background-color: blue");
-        label.setTextFill(Color.WHITE);
-        label.setTextAlignment(TextAlignment.CENTER);
-        ballPane.getChildren().add(label);
+        ballPane = new BallPane(); // Create a ball pane
 
-        ballPane.rateProperty().addListener(
-                (observableValue, oldRate, newRate) ->
-                        label.setText(String.format("%.2f", newRate))
-        );
+        speedLabel = new Label();
+        speedLabel.setPadding(new Insets(3, 10, 3, 10));
+        // styling label fx-css
+        speedLabel.setStyle("-fx-background-color: rgba(69,39,160, 0.35); -fx-font-weight: bold; " +
+                                    "-fx-background-radius: 0 0 10 10;");
 
-        // Pause and resume animation
-        ballPane.setOnMousePressed(e -> ballPane.pause());
-        ballPane.setOnMouseReleased(e -> ballPane.play());
+        speedLabel.setTextFill(Color.web("#311b92"));
+        speedLabel.setTextAlignment(TextAlignment.CENTER);
+        // makes ball visible (on top) when overlapped with the label
+        speedLabel.toBack();
 
-        // Increase and decrease animation
+        // to align label on center of the window HBox is used
+        HBox labelBox = new HBox(speedLabel);
+        labelBox.setAlignment(Pos.CENTER);
+
+        // put labelBox (HBox) on top side of the screen
+        ballPane.setTop(labelBox);
+
+        // on mouse press pause the animation
+        ballPane.setOnMousePressed(e -> {
+            ballPane.pause();
+            updateSpeedLabelText();
+        });
+
+        // on mouse press resume the animation
+        ballPane.setOnMouseReleased(e -> {
+            ballPane.play();
+            updateSpeedLabelText();
+        });
+
+        // when ball speed is changed update the text of the label
+        ballPane.setOnSpeedChanged(this::updateSpeedLabelText);
+
+        // increase and decrease animation speed
         ballPane.setOnKeyPressed(e -> {
             if (e.getCode() == KeyCode.UP) {
+                // ok key up increase the speed
                 ballPane.increaseSpeed();
             } else if (e.getCode() == KeyCode.DOWN) {
+                // on down decrease
                 ballPane.decreaseSpeed();
             }
         });
 
-        // Create a scene and place it in the stage
-        Scene scene = new Scene(ballPane, 250, 150);
+        // create a scene and place it in the stage
+        Scene scene = new Scene(ballPane, 400, 175);
         primaryStage.setTitle("Bounce Ball Control"); // Set the stage title
         primaryStage.setScene(scene); // Place the scene in the stage
         primaryStage.show(); // Display the stage
 
         // Must request focus after the primary stage is displayed
         ballPane.requestFocus();
-        ballPane.setStyle("-fx-background-color: white");
+        ballPane.setStyle("-fx-background-color: #ede7f6");
+
+        // initially set the text fir speed label
+        updateSpeedLabelText();
+    }
+
+    /**
+     * This method simply updates the label with current speed of the ball
+     */
+    private void updateSpeedLabelText() {
+        // check if ball is paused
+        if (ballPane.isPaused()) {
+            // ball is paused, the ball has no speed
+            speedLabel.setText("0.00");
+            return;
+        }
+
+        // update the label with current speed
+        speedLabel.setText(String.format("%.2f", ballPane.getSpeed()));
     }
 }
